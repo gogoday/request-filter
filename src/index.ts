0b07d0f92f035d2b44997597c96324ae580b6f14
sampsonwang <sampsonwang@tencent.com>
2019-07-23 14:39:24 +0800

    add

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as _ from 'lodash';
+
+/**
+ *  filter class
+ */
+class Filter {
+  // filter List
+  list: string[];
+
+  addKey(value) {
+    this.list.push(value)
+  }
+
+  hasKey(obj) {
+    if (_.isPlainObject(obj)) {
+      for (let i = 0, l = this.list.length; i < l; i ++ ) {
+        if (_.has(obj, this.list[i])) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+export default Filter
